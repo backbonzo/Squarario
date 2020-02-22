eacbc33ed6cc10617cf91834fed66faf5163ccab
Lemorz56 <sebbe_xbox@hotmail.com>
2020-02-23 00:30:09 +0100

    Minor changes

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -45,20 +45,19 @@ void getPos(sf::Vector2f& pos, int h, int w) {
 	do
 	{	pos.x = rand() % w;
 		pos.y = rand() % h;
-	} while ((!(pos.x > 10 && pos.x < 490) || !(pos.y > 10 && pos.y < 490)));
-
+	} while ((!(pos.x > 10 && pos.x < w - 10) || !(pos.y > 10 && pos.y < w - 10)));
 }
 
 int main() {
 	srand(time(NULL));
 
-	int H = 500, W = 500;
+	int H = 1024, W = 1024;
 
 	// create player instance 
 	Player player(sf::Vector2f(25.0f, 25.0f) , sf::Vector2f(H/2, W/2), sf::Color::Red);
 
 	//sf::Vector2f shapeSzie1(25, 25);
-	sf::RenderWindow window(sf::VideoMode(H, W), "mini game");
+	sf::RenderWindow window(sf::VideoMode(H, W), "Squarar.io");
 	//sf::RectangleShape shape1(shapeSzie1);
 	//sf::CircleShape  food(10.0f);
 	//shape1.setFillColor(sf::Color::Red);
@@ -68,16 +67,13 @@ int main() {
 
 	sf::Vector2f foodpos;
 
-	for (int i = 0; i < 1000; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		getPos(foodpos, H, W);
 		foodObj food(sf::CircleShape(5.0f), sf::Color::White, foodpos);
 		foods.push_back(food);
 	}
 
-	
-	
-	
 	bool eaten = false;
 
 	float movSpeed = 0.1f;
@@ -93,31 +89,27 @@ int main() {
 				window.close();
 
 			}
-
 		}
-
 
 		sf::Vector2f shapepos = player.getPlayerBlob().getPosition();
 
 		for (auto&& food: foods )
 		{
-		if (player.getPlayerBlob().getGlobalBounds().intersects(food.getFoodBlob().getGlobalBounds()))
-		{
-			eaten = true;
-			getPos(foodpos, H, W);
-			food.updatePos(foodpos);
-			player.setPlayerSpeed(player.getPlayerSpeed());
-			player.setPlayerSize(sf::Vector2f(player.getPlayerSize().x + 1.0f, player.getPlayerSize().y + 1.0f));
-			sf::RectangleShape p = player.getPlayerBlob();
-			p.setSize(player.getPlayerSize());
-			player.setPlayerBlob(p);
-
-		}
+			if (player.getPlayerBlob().getGlobalBounds().intersects(food.getFoodBlob().getGlobalBounds()))
+			{
+				eaten = true;
+				getPos(foodpos, H, W);
+				food.updatePos(foodpos);
+				player.setPlayerSpeed(player.getPlayerSpeed());
+				player.setPlayerSize(sf::Vector2f(player.getPlayerSize().x + 1.0f, player.getPlayerSize().y + 1.0f));
+				sf::RectangleShape p = player.getPlayerBlob();
+				p.setSize(player.getPlayerSize());
+				player.setPlayerBlob(p);
+			}
 		}
 
 		//moves player of wasd keys are pressed and making h, w as border for now
 		player.movePlayer(sf::Keyboard(), H, W);
-
 
 		window.clear();
 
